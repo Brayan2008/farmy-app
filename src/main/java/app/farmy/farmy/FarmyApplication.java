542725f94cf0e56648915032dff2fe63ca3fe5a8
Brayan2008 <bmaster20082005@gmail.com>
2025-11-06 20:37:45 -0500

    first commit

---
+++ src/main/java/app/farmy/farmy/FarmyApplication.java
@@ -0,0 +1,13 @@
+package app.farmy.farmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/farmy/farmy/FarmyApplicationTests.java
@@ -0,0 +1,13 @@
+package app.farmy.farmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
